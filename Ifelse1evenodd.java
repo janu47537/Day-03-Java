e0d4fa465badf5d02a1471ac8c4e946d11b5996a
janu47537 <janu47537@gmail.com>
2024-12-04 10:34:10 +0530

    Add files via upload

---
+++ Ifelse1evenodd.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Ifelse1evenodd{
+    public static void main(String[]args){
+        int a=3;
+        if((a&1)==0){
+            System.out.println("Even");
+            }else{
+                System.out.println("Odd");
+            }
+}}
